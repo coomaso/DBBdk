b6cd058047f0ba54484474892d1ce8aeaff975ae
coomaso <74811237+coomaso@users.noreply.github.com>
2025-03-20 14:56:26 +0800

    Update access_token.py

--- access_token.py
+++ access_token.py
@@ -231,7 +231,7 @@ class AuthClient:
             resp.raise_for_status()
             
             data = resp.json()["data"]["repData"]
-            logger.info(f"验证码数据: {data["jigsawImageBase64"]},{data["token"]},{data["secretKey"]}")
+            logger.info(f"验证码数据:{data["token"]}")
             return {
                 "client_uuid": client_uuid,
                 "secret_key": data["secretKey"],
