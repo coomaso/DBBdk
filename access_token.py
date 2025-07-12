579792d26d9f581b5c3b9b8593aeb029aa0c7043
coomaso <74811237+coomaso@users.noreply.github.com>
2025-07-12 10:04:11 +0800

    Update access_token.py

--- access_token.py
+++ access_token.py
@@ -17,8 +17,8 @@ from datetime import datetime, timedelta
 # 配置参数
 max_attempts = 10
 idCardSign = "MDoCAQEwEgIBATAKBggqgRzPVQFoAQoBAQMhALC5L1lSMTEQLmI33J1qUDVhRVwTyt%2Be%2B27ntIC3g2Wb"
-BASE_url = "https://zhcjsmz.sanxiacloud.com"
-login_url = "https://zhcjsmz.sanxiacloud.com/laboratt/attendance/page"
+BASE_url = "http://106.15.60.27:33333"
+login_url = "http://106.15.60.27:33333/laboratt/attendance/page"
 wexinqq_url = os.environ["QYWX_URL"]
 
 headers = {
@@ -30,8 +30,8 @@ headers = {
  "sec-ch-ua-mobile": "?0",
  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.289 Safari/537.36",
  "sec-ch-ua-platform": '"Windows"',
- "Origin": "https://zhcjsmz.sanxiacloud.com",
- "Referer": "https://zhcjsmz.sanxiacloud.com/login/",
+ "Origin": "http://106.15.60.27:33333",
+ "Referer": "http://106.15.60.27:33333/login/",
  "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,vi;q=0.7",
  "Accept-Encoding": "gzip, deflate",
  "Authorization": "Basic cGlnOnBpZw=="
@@ -253,7 +253,7 @@ def refresh_token():
     for attempt in range(1, max_attempts+1):
         logger.info(f"Token获取尝试第{attempt}次")
         session = requests.Session()
-        response = session.get("https://zhcjsmz.sanxiacloud.com/login/#/login", headers=headers)
+        response = session.get("http://106.15.60.27:33333/login/#/login", headers=headers)
         
         # 解析 Cookie
         cookies_dict = requests.utils.dict_from_cookiejar(session.cookies)
